e22be2b0ff9adc282820a331a811d2db30d8dacd
Kovinda Karunanayake <75525796+Kovinda@users.noreply.github.com>
2021-05-31 18:56:04 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1017,15 +1017,13 @@ function file_audio(path) {
                   base64String += String.fromCharCode(picture.data[i]);
               }
               imageUri = "data:" + picture.format + ";base64," + window.btoa(base64String);
-              console.log(imageUri);
-              console.log("success");
+              
+              
           },
           onError: function (error) {
-            imageUri = UI.audioposter;
             console.log(imageUri);
               // handle error
               console.log(error);
-              console.log("error");
           }
       });
     var content = `
@@ -1033,7 +1031,7 @@ function file_audio(path) {
   <div class="card" style="background-image: linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%);">
   <div class="card-body text-center">
   <div class="${UI.file_view_alert_class}" id="file_details" role="alert">${obj.name}<br>${size}</div>
-  <br><img draggable="false" src="${imageUri}" width="100%" /><br>
+  <br><img draggable="false" src="${(typeof variable === 'undefined')  ? UI.audioposter : imageUri}" width="100%" /><br>
   <audio id="vplayer" width="100%" playsinline controls>
     <source src="${url}" type="audio/ogg">
     <source src="${url}" type="audio/mpeg">
